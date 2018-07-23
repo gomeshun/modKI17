ca9720734d32ea854f04ed2691ff09af6b93086f
shunichi.horigome@ipmu.jp from sm_pc <shunichi.horigome@ipmu.jp>
2018-07-23 23:43:04 +0900

    update run.sh for KI17 test

--- python/run.sh
+++ python/run.sh
@@ -1,6 +1,6 @@
 v=v001
 
-for i in `seq 0 9`
+for i in `seq 0 49`
 do
-python parameter_estimation_mem.py mock_mem_noMCMC_${v}_${i}b.csv params_chain_noMCMC_${v}_${i}b.csv mclog_noMCMC_${v}_${i}b.csv --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > noMCMC_${v}_${i}b.log.txt &
+python parameter_estimation.py mock_tot_noMCMC_${v}_${i}.csv mock_tot_noMCMC_${v}_${i}.csv params_chain_noMCMC_tot_${v}_${i}.csv mclog_noMCMC_tot_${v}_${i}.csv --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > noMCMC_tot_${v}_${i}.log.txt &
 done
