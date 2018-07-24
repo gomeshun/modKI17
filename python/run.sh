fc1d790f19fab4044c3f50f9e927d3036a5b3d62
Shunichi_Horigome <shunichi.horigome@ipmu.jp>
2018-07-24 15:40:06 +0900

    sh for mod runs

--- python/run.sh
+++ python/run.sh
@@ -1,6 +1,52 @@
 v=v001
 
-for i in `seq 0 49`
+for i in `seq 0 9`
 do
-python parameter_estimation.py mock_tot_noMCMC_${v}_${i}.csv mock_tot_noMCMC_${v}_${i}.csv params_chain_noMCMC_tot_${v}_${i}.csv mclog_noMCMC_tot_${v}_${i}.csv --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > noMCMC_tot_${v}_${i}.log.txt &
+fnmem=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+fnfg=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+pchain=params_chain_noMCMC_plummerlike_tot_${v}_${i}.csv
+mclog=mclog_modKI17_noMCMC_plummerlike_tot_${v}_${i}.csv
+logtxt=modKI17_noMCMC_plummerlike_tot_${v}_${i}.log.txt
+python parameter_estimation.py ${fnmem} ${fnfg} ${pchain} ${mclog} --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > ${logtxt} &
 done
+wait
+for i in `seq 10 19`
+do
+fnmem=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+fnfg=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+pchain=params_chain_noMCMC_plummerlike_tot_${v}_${i}.csv
+mclog=mclog_modKI17_noMCMC_plummerlike_tot_${v}_${i}.csv
+logtxt=modKI17_noMCMC_plummerlike_tot_${v}_${i}.log.txt
+python parameter_estimation.py ${fnmem} ${fnfg} ${pchain} ${mclog} --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > ${logtxt} &
+done
+wait
+for i in `seq 20 29`
+do
+fnmem=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+fnfg=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+pchain=params_chain_noMCMC_plummerlike_tot_${v}_${i}.csv
+mclog=mclog_modKI17_noMCMC_plummerlike_tot_${v}_${i}.csv
+logtxt=modKI17_noMCMC_plummerlike_tot_${v}_${i}.log.txt
+python parameter_estimation.py ${fnmem} ${fnfg} ${pchain} ${mclog} --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > ${logtxt} &
+done
+wait
+for i in `seq 30 39`
+do
+fnmem=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+fnfg=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+pchain=params_chain_noMCMC_plummerlike_tot_${v}_${i}.csv
+mclog=mclog_modKI17_noMCMC_plummerlike_tot_${v}_${i}.csv
+logtxt=modKI17_noMCMC_plummerlike_tot_${v}_${i}.log.txt
+python parameter_estimation.py ${fnmem} ${fnfg} ${pchain} ${mclog} --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > ${logtxt} &
+done
+wait
+for i in `seq 40 49`
+do
+fnmem=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+fnfg=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+pchain=params_chain_noMCMC_plummerlike_tot_${v}_${i}.csv
+mclog=mclog_modKI17_noMCMC_plummerlike_tot_${v}_${i}.csv
+logtxt=modKI17_noMCMC_plummerlike_tot_${v}_${i}.log.txt
+python parameter_estimation.py ${fnmem} ${fnfg} ${pchain} ${mclog} --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > ${logtxt} &
+done
+wait
---
+++ python/run_plummerlike.sh
@@ -0,0 +1,16 @@
+v=v001
+
+for n in `seq 0 4`
+do
+  for j in `seq 0 9`
+  do
+    i=$((10*n+j))
+    fnmem=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+    fnfg=mock_tot_plummerlike_noMCMC_${v}_${i}.csv
+    pchain=params_chain_modKI17_noMCMC_plummerlike_tot_${v}_${i}.csv
+    mclog=mclog_modKI17_noMCMC_plummerlike_tot_${v}_${i}.csv
+    logtxt=modKI17_noMCMC_plummerlike_tot_${v}_${i}.log.txt
+    python parameter_estimation_mod.py ${fnmem} ${fnfg} ${pchain} ${mclog} --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > ${logtxt} &
+  done
+  wait
+done
