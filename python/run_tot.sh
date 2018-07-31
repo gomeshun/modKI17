240290401b478c8f0d35dd46eb0c439ae881d8e4
Shunichi_Horigome <shunichi.horigome@ipmu.jp>
2018-07-31 13:16:30 +0900

    v003

--- python/run_tot.sh
+++ python/run_tot.sh
@@ -10,10 +10,10 @@ do
     echo "calc start for ${i}th mock."
     fnmem=mock_tot_noMCMC_${v}_${i}.csv
     fnfg=mock_tot_noMCMC_${v}_${i}.csv
-    pchain=params_chain_modKI17_noMCMC_tot_${v}_${i}.csv
-    mclog=mclog_modKI17_noMCMC_tot_${v}_${i}.csv
-    logtxt=modKI17_noMCMC_tot_${v}_${i}.log.txt
-    python parameter_estimation_mod.py ${fnmem} ${fnfg} ${pchain} ${mclog} --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > ${logtxt} &
+    pchain=params_chain_KI17_noMCMC_tot_${v}_${i}.csv
+    mclog=mclog_KI17_noMCMC_tot_${v}_${i}.csv
+    logtxt=KI17_noMCMC_tot_${v}_${i}.log.txt
+    python parameter_estimation.py ${fnmem} ${fnfg} ${pchain} ${mclog} --no-confirmation --burnin 5000 --burnin-repeat 3 --sampling 20000 > ${logtxt} &
   done
   wait
 done
